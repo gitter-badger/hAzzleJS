d16c74da1dca53fca1e4f1d8b0c24b23127978a1
KFlash <kflash123@gmail.com>
2014-11-09 08:21:27 +0800

    cleanup

--- distro/hazzle.js
+++ distro/hazzle.js
@@ -1,58 +1,61 @@
 /*!
  * hAzzle.js
  * Copyright (c) 2014 Kenny Flashlight
- * Version: 1.0.1a
+ * Version: 1.0.0d Release Candidate
  * Released under the MIT License.
  *
- * Date: 2014-11-08
+ * Date: 2014-11-09
  */
+ 
 (function() {
 
     var
-    // Quick-lookup for hAzzle(id)
 
-        idOnly = /^#([\w\-]*)$/,
-
-        // Holder for all modules
+        // Container for all modules
 
         modules = {},
 
-        // Keep track of installed modules. Hopefully people won't spoof this... would be daft.
+        // Container for installed modules
 
         installed = {},
-
+       
+        // Version
+        
         version = '1.0.0a-rc',
 
-        codename = 'new-age',
+       // Codename
+       
+       codename = 'new-age',
 
         // Throws an error if `condition` is `true`.
 
         err = function(condition, code, message) {
-            if (condition) {
-                var e = new Error('[hAzzle-' + code + '] ' + message);
-                e.code = code;
-                throw e;
-            }
+          if(condition) {
+               throw new Error('[hAzzle-' + code + '] ' + message);
+               }
         },
-        // Returns an instance for `name`
+        
+        // Lower case 
+        
+        tlc = function(str) {
+          return str.toLowerCase();
+        },
+        
+        // Require a module
 
         require = function(name) {
-            return modules[name];
+          if(name && typeof name === 'string') {
+            return modules[tlc(name)];
+          }  
         },
 
-        // Defines a module for `name: String`, `fn: Function`,
+        // Defines a module
 
         define = function(name, fn) {
-
-            // Check arguments
-            err(typeof name !== 'string', 1, 'id must be a string "' + name + '"');
-            err(modules[name], 2, 'module already included "' + name + '"');
-            err(typeof fn !== 'function', 3, 'function body for "' + name + '" must be an function "' + fn + '"');
-
-            // append to module object
-            installed[name] = true;
-
-            modules[name] = fn.call(hAzzle.prototype);
+         if(typeof name === 'string' && typeof fn === 'function' && !modules[name]) {
+            installed[tlc(name)] = true;
+            modules[tlc(name)] = fn.call(hAzzle.prototype);
+           } 
         },
 
         validTypes = function(elem) {
@@ -60,9 +63,6 @@
         },
 
         // Define a local copy of hAzzle
-        // NOTE! Everything need to be returned as an array
-        // so important to wrap [] around the 'sel' to avoid
-        // errors
 
         hAzzle = function(sel, ctx) {
 
@@ -82,10 +82,10 @@
             // Include required module
 
             var m, els, _util = hAzzle.require('Util'),
-                // Document ready
+               // Document ready
                 _ready = hAzzle.require('Ready');
 
-            // If a function is given, call it when the DOM is ready
+            // DOM ready
 
             if (typeof sel === 'function') {
                 if (installed.Ready) {
@@ -96,20 +96,7 @@
             }
 
             if (typeof sel === 'string') {
-
-                // Quick look-up for hAzzle(#id)
-
-                if ((m = idOnly.exec(sel)) && !ctx) {
-                    els = [document.getElementById(m[1])];
-                }
-
-                if (els === null || els === undefined) {
-
-                    // The 'find' method need to have a boolean value set to 'true', to 
-                    // work as expected. Else it will behave like the global .find method
-
                     els = this.find(sel, ctx, true);
-                }
                 // hAzzle([dom]) 
             } else if (sel instanceof Array) {
                 els = _util.unique(_util.filter(sel, validTypes));
@@ -133,9 +120,6 @@
             }
 
             // Create a new hAzzle collection from the nodes found
-            // NOTE!! If undefined, set length to 0, and
-            // elements to an empty array [] to avoid hAzzle
-            // throwing errors
 
             if (els === undefined) {
                 this.length = 0;
