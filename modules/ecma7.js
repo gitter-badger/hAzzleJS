23b8466a82fc91bd5e5a9e2a23bb34d8a4528f03
KFlash <kflash123@gmail.com>
2014-11-15 10:20:46 +0800

    cleanup

--- modules/ecma7.js
+++ modules/ecma7.js
@@ -3,12 +3,16 @@
  *
  * Note! ECMA 7 ( ecma262 ) not supported by the browsers!
  *
- * Ref: https://github.com/tc39/ecma262
+ * Ref: https://people.mozilla.org/~jorendorff/es6-draft.html
+ *
+ * Current proposals: https://github.com/tc39/ecma262
+ *
+ * NOTE! If you want to add something here, make sure it's has a
+ * value for hAzzle - e.g. many devs can have benefits of it in writing modules
  *
  * This shims contains:
  * --------------------
  *
- * - values
  * - entries
  * - escape
  * - compare
@@ -22,10 +26,10 @@ var _isNaN = function(value) {
         // `NaN` as a primitive is the only value that is not equal to itself
         return typeof value === 'number' && value != +value;
     },
-    ObjectCreate = function(proto, data) {
-        return Object.create(proto, data);
+    ObjectCreate = function(proto, internalSlotsList) {
+        return Object.create(proto, internalSlotsList);
     },
-
+    // Ecma 7.1.13
     ToObject = function(v) {
         if (v === null || v === undefined) {
             throw TypeError();
@@ -43,18 +47,58 @@ var _isNaN = function(value) {
         return ((n < 0) ? -1 : 1) * Math.floor(Math.abs(n));
     };
 
-if (![].values) {
-    Object.defineProperty(Array.prototype, 'values', {
+// ABSTRACT OPERATIONS 
+
+// Ecma 7.1.13 
+if (![].ToObject) {
+    Object.defineProperty(Array.prototype, 'ToObject', {
         enumerable: false,
         configurable: true,
         writable: true,
-        value: function(o) {
-            return Object.keys(o).map(function(p) {
-                return o[p];
-            });
+        value: ToInteger
+    });
+}
+
+// Ecma 7.1.4
+if (![].ToInteger) {
+    Object.defineProperty(Array.prototype, 'ToInteger', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: ToInteger
+    });
+}
+
+// Ecma 7.1.5
+if (![].ToLength) {
+    Object.defineProperty(Array.prototype, 'ToLength', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function(v) {
+
+            var len = ToInteger(v);
+            if (len <= 0) {
+                return 0;
+            }
+            return Math.min(len, 0x20000000000000 - 1); // 2^53-1
         }
     });
 }
+
+
+// Ecma 7.1.6
+if (![].ToUint32) {
+    Object.defineProperty(Array.prototype, 'ToUint32', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function(v) {
+            return v >>> 0;
+        }
+    });
+}
+
 // http://esdiscuss.org/topic/regexp-escape
 if (![].escape) {
     Object.defineProperty(Array.prototype, 'escape', {
