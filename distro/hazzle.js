ed528ff4c60f81ed1c3ad796867a6dd2cdb598cd
KFlash <kflash123@gmail.com>
2014-11-17 18:15:21 +0800

    fixed units and minor issues

--- distro/hazzle.js
+++ distro/hazzle.js
@@ -64,7 +64,7 @@
         // Define a local copy of hAzzle
 
         hAzzle = function(sel, ctx) {
-
+alert(sel)
             // hAzzle(), hAzzle(null), hAzzle(undefined), hAzzle(false)
             if (!sel) {
                 return;
@@ -95,6 +95,7 @@
                     sel[0] === '<' &&
                     sel[sel.length - 1] === '>' &&
                     sel.length >= 3) {
+                        alert(sel)
                     els = require('manipulation').create(
                         sel,
                         ctx && ctx.nodeType ? ctx.ownerDocument || ctx : document
@@ -2183,16 +2184,13 @@ hAzzle.define('css', function() {
         inline = ('b big i small abbr acronym cite code dfn em kbd strong samp var a bdo br img map' +
             'object q script span sub button input label select textarea').split(' '),
 
-        // Various Regexes
+        runit = /^(-?[\d+\.\-]+)([a-z]+|%)$/i,
+        docElement = document.documentElement,
+        computedValueBug,
 
-        widthHeight = /^(width|height)$/,
-        listitem = /^(li)$/i,
-        tablerow = /^(tr)$/i,
-        table = /^(table)$/i,
-        margin = (/^margin/),
-        units = /^([+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|))(?!px)[a-z%]+$/i,
+        convert = {};
 
-        computedValues = function(elem) {
+    var computedValues = function(elem) {
             // Avoid getting style values on the window object
             if (elem && elem.nodeType && elem.ownerDocument !== null) {
                 var view = false;
@@ -2213,6 +2211,7 @@ hAzzle.define('css', function() {
             }
             return '';
         },
+
         computed = function(elem) {
 
             if (elem) {
@@ -2227,6 +2226,7 @@ hAzzle.define('css', function() {
                 return storage.private.get(elem, 'css');
             }
         },
+
         getStyles = function(elem) {
             return elem && (computed(elem).computedStyle === undefined || computed(elem).computedStyle == null) ?
                 computed(elem).computedStyle = computedValues(elem) :
@@ -2238,6 +2238,8 @@ hAzzle.define('css', function() {
             // to round for integer pixels.
             return parseFloat(value) || 0;
         },
+
+        // Return the computed value of a CSS property
         getDisplay = function(elem) {
             if (elem) {
                 var tagName = elem.tagName.toLowerCase();
@@ -2246,15 +2248,15 @@ hAzzle.define('css', function() {
                     return 'inline';
                 }
 
-                if (listitem.test(tagName)) {
+                if (tagName === 'li') {
                     return 'list-item';
                 }
 
-                if (tablerow.test(tagName)) {
+                if (tagName === 'tr') {
                     return 'table-row';
                 }
 
-                if (table.test(tagName)) {
+                if (tagName === 'table') {
                     return 'table';
                 }
             }
@@ -2266,7 +2268,7 @@ hAzzle.define('css', function() {
             if (elem === null || elem === undefined) {
                 return;
             } else {
-                elem = elem != null && elem instanceof hAzzle ? elem.elements : elem.length ? elem[0] : elem
+                elem = elem != null && elem instanceof hAzzle ? elem.elements : elem.length ? elem[0] : elem;
             }
 
             // Avoid hAzzle from throwing errors if the element doesn't exist
@@ -2275,14 +2277,19 @@ hAzzle.define('css', function() {
                 return;
             }
 
-            var ret = 0;
+            var value = 0,
+                unit,
+                outerProp = ['paddingTop', 'paddingBottom', 'borderTop', 'borderBottom'],
+                innerHeight,
+                parent,
+                i = 4; // outerProp.length
 
-            if (widthHeight.test(prop) && css(elem, 'display') === 0) {
+            if ((prop === 'width' || prop === 'height') && css(elem, 'display') === 0) {
                 elem.style.display = 'none';
                 elem.style.display = getDisplay(elem);
             }
 
-            if (!force || force === undefined) { 
+            if (!force || force === undefined) {
                 // NOTE! hAzzle will throw 'too much recusion' if this get screwed up
                 if (prop === 'height' &&
                     css(elem, 'boxSizing') !== 'border-box') {
@@ -2300,9 +2307,15 @@ hAzzle.define('css', function() {
                         (toPixel(css(elem, 'paddingRight'))) + 'px';
                 }
             }
+
             var computedStyle = getStyles(elem);
 
-            if (computedStyle) {
+            if (prop === 'fontSize') {
+                // correct IE issues with font-size
+                // @see http://bugs.jquery.com/ticket/760
+                value = toPx(elem, '1em', 'left', 1) + 'px';
+
+            } else if (computedStyle) {
 
                 // IE and Firefox do not return a value for the generic borderColor -- they only return 
                 // individual values for each border side's color.
@@ -2314,48 +2327,120 @@ hAzzle.define('css', function() {
                 // Support: IE9
 
                 if (feature.ie === 9 && prop === 'filter') {
-                    ret = computedStyle.getPropertyValue(prop);
+                    value = computedStyle.getPropertyValue(prop);
                 } else {
-                    ret = computedStyle[prop];
+                    value = computedStyle[prop];
                 }
 
                 // Fall back to the property's style value (if defined) when 'ret' returns nothing
 
-                if (ret === '' && !hAzzle.require('Core').contains(elem.ownerDocument, elem)) {
-                    ret = elem.style[prop];
-                }
-
-                // Support: Android 4.0-4.3
-
-                if (feature.has('mobile') && feature.has('android')) {
-
-                    if (units.test(ret) && margin.test(name)) {
-
-                        var width, minWidth, maxWidth,
-                            style = elem.style;
-                        // Remember the original values
-                        width = style.width;
-                        minWidth = style.minWidth;
-                        maxWidth = style.maxWidth;
-
-                        // Put in the new values to get a computed value out
-                        style.minWidth = style.maxWidth = style.width = ret;
-                        ret = computed.width;
-
-                        // Revert the changed values
-                        style.width = width;
-                        style.minWidth = minWidth;
-                        style.maxWidth = maxWidth;
-                    }
+                if (value === '' && !hAzzle.require('Core').contains(elem.ownerDocument, elem)) {
+                    value = elem.style[prop];
                 }
             }
-            return ret !== undefined ? ret + '' : ret;
+
+            // check the unit
+            unit = (value.match(runit) || [])[2];
+
+            if (unit === '%' && computedValueBug) {
+                // WebKit won't convert percentages for top, bottom, left, right, margin and text-indent
+                if (prop === 'top' || prop === 'bottom') {
+                    // Top and bottom require measuring the innerHeight of the parent.
+                    innerHeight = (parent = elem.parentNode || elem).offsetHeight;
+                    while (i--) {
+                        innerHeight -= parseFloat(css(parent, outerProp[i]));
+                    }
+                    value = parseFloat(value) / 100 * innerHeight + 'px';
+                } else {
+                    value = toPx(elem, value);
+                }
+            }
+
+            if ((value === 'auto' || (unit && unit !== 'px'))) {
+                // WebKit and Opera will return auto in some cases
+                // Firefox will pass back an unaltered value when it can't be set, like top on a static element
+                value = 0;
+            }
+
+            return value !== undefined ? value + '' : value;
+        },
+
+        // Convert a value to pixels
+
+        toPx = function(elem, value, prop, force) {
+
+            prop = prop || 'width';
+
+            var style,
+                inlineValue,
+                ret,
+                unit = (value.match(runit) || [])[2],
+                conversion = unit === 'px' ? 1 : convert[unit + 'toPx'],
+                rem = /r?em/i;
+            if (conversion || rem.test(unit) && !force) {
+                // calculate known conversions immediately
+                // find the correct element for absolute units or rem or fontSize + em or em
+
+                elem = conversion ? elem : unit === 'rem' ?
+                    docElement : prop === 'fontSize' ?
+                    elem.parentNode || elem : elem;
+
+                // Use the pre-calculated conversion or fontSize of the element for rem and em
+                conversion = conversion || parseFloat(css(elem, 'fontSize'), true);
+
+                // multiply the value by the conversion
+                ret = parseFloat(value) * conversion;
+
+                // Support: Android 4.0-4.3 
+            } else {
+
+                style = elem.style;
+
+                inlineValue = style[prop];
+
+                // set the style on the target element
+
+                style[prop] = value;
+
+                // read the computed value
+                // if style is nothing we probably set an unsupported unit
+                ret = !style[prop] ? 0 : parseFloat(css(elem, prop, true));
+
+                // reset the style back to what it was or blank it out
+                style[prop] = inlineValue !== undefined ? inlineValue : null;
+            }
+
+            // return a number
+            return ret;
         };
+
+    (function() {
+
+        var conversions = [1 / 25.4, 1 / 2.54, 1 / 72, 1 / 6],
+            units = ['mm', 'cm', 'pt', 'pc', 'in', 'mozmm'],
+            i = units.length, // units.length
+            div = document.createElement('div');
+
+        docElement.appendChild(div);
+        div.style.marginTop = '1%';
+        computedValueBug = computedValues(div).marginTop === '1%';
+
+        // Pre-calculate absolute unit conversions
+
+        while (i--) {
+            convert[units[i] + 'toPx'] = conversions[i] ? conversions[i] * convert.inToPx : toPx(div, '1' + units[i]);
+        }
+
+        docElement.removeChild(div);
+        // Avoid memory leaks in IE
+        div = null;
+    }());
 
     return {
         computed: computed,
         display: getDisplay,
         styles: getStyles,
+        toPx: toPx,
         css: css
     };
 });
