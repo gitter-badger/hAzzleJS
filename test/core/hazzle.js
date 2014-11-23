cb49f602c76e2f5e4c20cd463231b4fd7a76dc84
KFlash <kflash123@gmail.com>
2014-11-23 20:58:41 +0800

    typo

--- test/core/hazzle.js
+++ test/core/hazzle.js
@@ -8,7 +8,7 @@ describe('hAzzle()', function () {
 
   it('hAzzle.version', function () {
     expect(hAzzle.version).to.be.a('string');
-    expect(hAzzle.version).to.equal('1.0.1b');
+    expect(hAzzle.version).to.equal('1.0.2b');
   });
  /* 
   it('hAzzle.err', function () {
